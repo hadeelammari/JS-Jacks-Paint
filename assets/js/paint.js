0ddd776ec56506b1e51b241df79f32c682a5b397
ZvsZig <142462854+ZvsZig@users.noreply.github.com>
2023-10-18 17:55:31 -0700

    comments to show understanding

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,13 +1,18 @@
 function configureListeners() {
+    //reaches into the html document and finds the elements with the "img"
+    //this allows the html element to be edited and called in JavaScript
     let images = document.getElementsByTagName('img'); 
 
-
+    //this for loop acts as a giant boolean statement that calls the "addOpacity" and "removeOpacity"
+    //the "event listeners" are built in methods that wait for certain goals to be met before they activate
+    //For each "color image", listen for the event  
      for (var i = 0; i < images.length; i++) {        
         document.getElementById(images[i].id).addEventListener('mouseover', addOpacity, false)        
         document.getElementById(images[i].id).addEventListener('mouseout', removeOpacity, false)      
     } 
 }
 
+//function that when called wait for the users mouse to hover over the target color and "dim" to change the opacity
 function addOpacity(event) {
     if (!this.classList.contains('dim')){
         this.classList.add('dim')
@@ -15,24 +20,27 @@ function addOpacity(event) {
     getProductInfo(event.target.id);     
 }
 
+//function that when called waits for the users mouse to stop hovering the target color to change the color back 
 function removeOpacity(event) {
     if (this.classList.contains('dim')){
         this.classList.remove('dim');
     }
-
+//placeholde/unneeded
     let element = document.getElementById('color-price');
         element.textContent = '';
         
     let color = document.getElementById('color-name');
         color.textContent = ''; 
-
+//prevents default propogation to prevent all elements being affected by one change
     event.preventDefault();    
 }
 
+//creates class with parameters which change depending on ht elementbeing selected
 function getProductInfo(paintColor) {
     let price;
     let colorName;  
     
+//when switch case is used, the switch is called once, and case is used for every instance and runs when if it matched 
     switch (paintColor) {
         case 'pn1':           
             colorName = 'Lime Green';
@@ -82,9 +90,11 @@ function getProductInfo(paintColor) {
           default:              
     }
 
+    //function that finds and changes the html element with the matching id 
     function updatePrice(colorName, price)
     {       
         let colorPrice = document.getElementById('color-price')
+        //x.textContent is the tag that changes the text content on the html file
         colorPrice.textContent = price;
         
         let color =  document.getElementById('color-name')
